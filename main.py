9323cb8d8d3212043ec3baecc1bb70e410551705
Meshek Soren <mesheksoren@gmail.com>
2024-02-24 18:41:38 -0600

    Rebase

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pygame
 import time
 import random
@@ -18,3 +19,24 @@ def main():
 
 if __name__ == '__main__':
     main()
+=======
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT  = 500, 400
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("space Dodge")
+
+def main():
+    run = True
+    while run:
+        for event  in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break 
+    pygame.quit 
+if __name__=="__main__":
+    main()
+    
+>>>>>>> Stashed changes
